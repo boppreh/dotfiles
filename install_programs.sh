ef0a273b575996d55290345a4db0be5d9c0b12b5
BoppreH <boppreh@gmail.com>
2015-09-07 21:07:55 -0300

    Add JQ to installed programs

--- install_programs.sh
+++ install_programs.sh
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 sudo $MANAGER update
-sudo $MANAGER install screen vim git golang
+sudo $MANAGER -y install screen vim git golang
 
 if ! hash python3 2>/dev/null; then
     wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tar.xz
@@ -22,6 +22,18 @@ if ! hash python3 2>/dev/null; then
     rm Python-3.4.3.tar.xz 
     # Requires sudo because "sudo make install" created some root-owned files.
     sudo rm -r Python-3.4.3
+fi
+
+if ! hash jq 2>/dev/null; then
+    wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz
+    tar -xzvf jq-1.5.tar.gz
+
+    cd jq-1.5
+    ./configure && make && sudo make install
+    cd ..
+
+    rm jq-1.5.tar.gz
+    rm -r jq-1.5
 fi
 
 sudo pip3 install --upgrade pip
