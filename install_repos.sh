5ca294cf115d393b65dbf22a85de51f53d64eccb
BoppreH <boppreh@gmail.com>
2015-11-04 19:57:29 -0200

    Increase number of repos checked for cloning

--- install_repos.sh
+++ install_repos.sh
@@ -1,6 +1,6 @@
 TARGET=${1:-$HOME}
 cd $TARGET
-curl https://api.github.com/users/boppreh/repos | jq '.[] | .ssh_url' | xargs -I{} git clone {}
+curl https://api.github.com/users/boppreh/repos?per_page=200 | jq '.[] | .ssh_url' | xargs -I{} git clone {}
 echo 'To clone private repos, enter your access token ( https://github.com/settings/tokens ): '
 read ACCESS_TOKEN
 curl "https://api.github.com/user/repos?type=private&access_token=$ACCESS_TOKEN" | jq '.[] | .ssh_url' | xargs -I{} git clone {}
