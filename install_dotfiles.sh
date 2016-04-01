623cb544d41897006e4f911a778e3c1577ea27f4
BoppreH <boppreh@gmail.com>
2016-04-01 16:31:33 +0000

    Draft conky installation

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -7,6 +7,8 @@ ln -vsf "`pwd`/.pythonrc.py" $HOME/
 ln -vsf "`pwd`/.screenrc" $HOME/
 ln -vsf "`pwd`/.vim" $HOME/
 ln -vsf "`pwd`/.vimrc" $HOME/
+#ln -vsf "`pwd`/.conkyrc" $HOME/
+#ln -vsf "`pwd`/i3config" $HOME/.config/i3/config
 if hash startx 2>/dev/null; then
     ln -vsf "`pwd`/user" $HOME/.config/dconf
 fi
